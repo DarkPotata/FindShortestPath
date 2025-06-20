495d47d138c0dc897b9ba0005a7b749ad5a8839f
Sergey <serega.truhmanoff@yandex.com>
2025-06-20 17:49:49 +0300

    second version

--- test_algoritmdejcstra.h
+++ test_algoritmdejcstra.h
@@ -1,10 +1,27 @@
 #ifndef TEST_ALGORITMDEJCSTRA_H
 #define TEST_ALGORITMDEJCSTRA_H
 
-class test_algoritmDejcstra
+#include <QObject>
+#include <QMap>
+#include <QVector>
+#include <QSet>
+#include <QDebug>
+
+class TestDijkstraAlgorithm : public QObject
 {
-public:
-    test_algoritmDejcstra();
+    Q_OBJECT
+
+private slots:
+    void testMinimalGraph();
+    void testMultiplePathsSelectShortest();
+    void testNoPathExists();
+    void testIsolatedStartCity();
+    void testGraphWithCycles();
+    void testLargeGraph();
+
+private:
+    // Только объявление, без определения
+    QMap<char, QMap<char, int>> createTestMatrix(const QVector<QVector<int>>& data, const QVector<char>& cities);
 };
 
 #endif // TEST_ALGORITMDEJCSTRA_H
