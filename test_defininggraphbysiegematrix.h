ff72e5ec878cd272ee7f01cc815ebe70cd84b2e0
Sergey <serega.truhmanoff@yandex.com>
2025-06-20 21:44:49 +0300

    second version

--- test_defininggraphbysiegematrix.h
+++ test_defininggraphbysiegematrix.h
@@ -1,10 +1,28 @@
-#ifndef TEST_DEFININGGRAPHBYSIEGEMATRIX_H
-#define TEST_DEFININGGRAPHBYSIEGEMATRIX_H
+#ifndef TEST_GRAPH_PARSER_H
+#define TEST_GRAPH_PARSER_H
 
-class test_definingGraphBysiegeMatrix
+#include <QObject>
+#include <QMap>
+#include <QVector>
+
+
+class TestGraphParser : public QObject
 {
-public:
-    test_definingGraphBysiegeMatrix();
+    Q_OBJECT
+private slots:
+    void testEmptyGraph();
+    void testNoCitiesDeclared();
+    void testInvalidCityNames();
+    void testInvalidDistanceFormat();
+    void testMissingDistance();
+    void testNoGraphDeclaration();
+    void testGraphLoop();
+    void testTableContentsError();
+    void testTooManyCities();
+    void testMinimalGraph();
+    void testDisconnectedGraph();
+    void testCyclicGraph();
+    void testMaxCitiesGraph();
 };
 
-#endif // TEST_DEFININGGRAPHBYSIEGEMATRIX_H
+#endif // TEST_GRAPH_PARSER_H
