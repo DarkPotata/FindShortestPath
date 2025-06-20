9b72d51a5a5705fa92494b70b6640b79c80fe14f
Sergey <serega.truhmanoff@yandex.com>
2025-06-20 19:34:46 +0300

    second version

--- test_createdotwithpath.h
+++ test_createdotwithpath.h
@@ -1,10 +1,21 @@
+// test_createdotwithpath.h
 #ifndef TEST_CREATEDOTWITHPATH_H
 #define TEST_CREATEDOTWITHPATH_H
 
-class test_createDotWithPath
-{
-public:
-    test_createDotWithPath();
-};
+#include <QObject>
+#include <QMap>
+#include <QVector>
 
-#endif // TEST_CREATEDOTWITHPATH_H
+class TestCreateDotWithPath : public QObject
+{
+    Q_OBJECT
+private slots:
+    void testMinimalGraph();
+    void testMultiplePaths();
+    void testNoPath();
+    void testLargeGraph();
+
+private:
+    QMap<char, QMap<char, int>> createTestMatrix(const QVector<QVector<int>>& data, const QVector<char>& cities);
+};
+#endif
